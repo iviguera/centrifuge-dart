bac78ae0ef6814c085b4f662c66b3649f7892bd1
iviguera <ivanviguera@gmail.com>
2019-10-06 22:04:59 +0200

    eliminate dart.io block

--- lib/centrifuge.dart
+++
@@ -1,5 +0,0 @@
-export 'src/client.dart' show Client, createClient;
-export 'src/client_config.dart';
-export 'src/error.dart';
-export 'src/events.dart';
-export 'src/subscription.dart' show Subscription;
