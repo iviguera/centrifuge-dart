0b116a2fac20b46db6ad55858102ed46b2619b7d
iviguera <ivanviguera@gmail.com>
2019-10-06 21:12:27 +0200

    preparing files

---
+++ lib/centrifuge_html.dart
@@ -0,0 +1,5 @@
+export 'src/client_web.dart' show Client, createClient;
+export 'src/client_config.dart';
+export 'src/error.dart';
+export 'src/events.dart';
+export 'src/subscription.dart' show Subscription;
